a9d1c1f03472ae8c2125b75dde6920cfaa5dfef3
Paul Butcher <paul@paulbutcher.com>
2021-10-02 15:37:38 +0100

    Initial checkin

---
+++ project.clj
@@ -0,0 +1,2 @@
+; Unused - only here because the Heroku Clojure buildpack insists upon project.clj being present
+{}
---
+++ test/clj/race_and_improve/session_test.clj
@@ -0,0 +1,5 @@
+(ns race-and-improve.session-test
+  (:require [clojure.test :refer [deftest is]]))
+
+(deftest simple-test
+  (is (= (+ 1 2) 3)))
